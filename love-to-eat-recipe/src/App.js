89038f589be49783ad9a0ebb2cc92dcae93a082e
TeeKenLau <dknight1938@hotmail.com>
2017-09-01 00:23:48 +1000

    Initial commit

---
+++ love-to-eat-recipe/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Food Recipe.</a>
+                    </div>
+                    <div id="navbar" className="navbar-collapse collapse">
+                        <ul className="nav navbar-nav">
+                            <li className="active"><a href="#">Home</a></li>
+                            <li><a href="#">Submit a recipe</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ love-to-eat-recipe/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+import Home from './Home';
+import Submit from '../src/Submit';
+
+ReactDOM.render(
+    <Router history={ browserHistory }>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path="submit" component={Submit}/>
+        </Route>
+    </Router>, document.getElementById('root'));
+
+registerServiceWorker();
+
